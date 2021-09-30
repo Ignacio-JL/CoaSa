58c8cfdd46ac83fcd06ad1cb1c3ab3dbecef8a9e
IgnacioPC <joseignacio439@gmail.com>
2021-09-30 11:45:34 -0300

    Api ok, prev to _mvc

---
+++ DatabaseAPI/Data/DatabaseAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DatabaseAPI.Model;
+
+namespace DatabaseAPI.Data
+{
+    public class DatabaseAPIContext : DbContext
+    {
+        public DatabaseAPIContext (DbContextOptions<DatabaseAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DatabaseAPI.Model.Usuarios> Usuarios { get; set; }
+    }
+}
---
+++ DatabaseAPI/Model/Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseAPI.Model
+{
+    public class Usuarios
+    {
+        public int id { get; set; }
+
+        public String UserName { get; set; }
+
+        public String Nombre { get; set; }
+
+        public String Email { get; set; }
+
+        public String Telefono { get; set; }
+    }
+}
